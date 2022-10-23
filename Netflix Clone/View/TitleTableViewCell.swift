a561661377084d8d05791e1915e8cbe1ceea602d
Toan <chan.misha95@mail.ru>
2022-10-23 10:37:24 +0300

    - Добавил отображение постеров и названий фильмов в  UpcomingView

---
+++ Netflix Clone/View/TitleTableViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by c.toan on 23.10.2022.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+   static let identifier = "TitleTableViewCell"
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.circle"), for: <#T##UIControl.State#>)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        let titlePosterUIImageViewConstraints = [
+            titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        let titleLabelConstraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: titlePosterUIImageView.trailingAnchor, constant: 20),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
+        let playTitleButtonConstraints = [
+            
+        ]
+        NSLayoutConstraint.activate(titlePosterUIImageViewConstraints)
+        NSLayoutConstraint.activate(titleLabelConstraints)
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        guard let url = URL(string:"https://image.tmdb.org/t/p/w500\(model.posterURL)" ) else { return }
+        titlePosterUIImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by c.toan on 23.10.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
